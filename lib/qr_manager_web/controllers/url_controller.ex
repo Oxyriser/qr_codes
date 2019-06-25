830c0e6ddc36d9c2c5be230d03abf03bf8bb9954
Ulysse Demay <u.demay@gmail.com>
2019-06-25 15:40:21 +0200

    add script jquery

--- lib/qr_manager_web/controllers/url_controller.ex
+++ lib/qr_manager_web/controllers/url_controller.ex
@@ -26,15 +26,10 @@ defmodule QrManagerWeb.URLController do
 
   def create(conn, %{"url" => url_params}) do
     user = conn.assigns[:user]
-    case URLManager.create_url(Map.put(url_params, "user_id", user.id)) do
-      {:ok, url} ->
-        conn
-        |> put_flash(:info, "Url created successfully.")
-        |> redirect(to: Routes.url_path(conn, :show, url.id))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
+    {:ok, url} = URLManager.create_url(Map.put(url_params, "user_id", user.id))
+      conn
+      |> put_flash(:info, "Url created successfully.")
+      |> redirect(to: Routes.url_path(conn, :show, url.id))
   end
 
   def show(conn, %{"id" => id}) do
@@ -50,16 +45,13 @@ defmodule QrManagerWeb.URLController do
 
   def update(conn, %{"id" => id, "url" => url_params}) do
     url = URLManager.get_url!(id)
+    {:ok, url} = URLManager.update_url(url, url_params)
+    conn
+    |> put_flash(:info, "Url updated successfully.")
+    |> redirect(to: Routes.url_path(conn, :show, url.id))
 
-    case URLManager.update_url(url, url_params) do
-      {:ok, url} ->
-        conn
-        |> put_flash(:info, "Url updated successfully.")
-        |> redirect(to: Routes.url_path(conn, :show, url.user_id, url.id))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", url: url, changeset: changeset)
-    end
+      # {:error, %Ecto.Changeset{} = changeset} ->
+      #   render(conn, "edit.html", url: url, changeset: changeset)
   end
 
   def delete(conn, %{"id" => id}) do
