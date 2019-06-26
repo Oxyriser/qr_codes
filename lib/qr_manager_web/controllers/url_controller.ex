92fd9fd05d42d5daf7e432ce39bf713ffeadf398
Theo Danneels <theo.danneels@telecom-paristech.fr>
2019-06-26 14:32:49 +0200

    Fix url update

--- lib/qr_manager_web/controllers/url_controller.ex
+++ lib/qr_manager_web/controllers/url_controller.ex
@@ -8,24 +8,16 @@ defmodule QrManagerWeb.URLController do
 
   def index(conn, _params) do
     user = conn.assigns[:user]
-    IO.inspect(user)
     if user != nil do
       urls = URL |> Ecto.Query.where(user_id: ^user.id) |> QrManager.Repo.all()
       json(conn, %{"liste" => Enum.map(urls, &extract/1)})
     else
-      conn
-      |> send_resp(401, "You are not connected")
+      conn |> send_resp(401, "You are not connected")
     end
   end
 
   defp extract(%QrManager.URLManager.URL{url: url, id: id}) do
     %{url: url, id: "https://qrmanager.rfc1149.net/redirect/" <> id}
-  end
-
-  def new(conn, _params) do
-    user = conn.assigns[:user]
-    changeset = URLManager.change_url(%URL{})
-    render(conn, "new.html", user_id: user.id, changeset: changeset)
   end
 
   def create(conn, params) do
@@ -38,33 +30,24 @@ defmodule QrManagerWeb.URLController do
 
   def show(conn, %{"id" => id}) do
     user = conn.assigns[:user]
+    IO.puts "Je suis deja passe par la"
     if user != nil do
       user_id = user.id
       url = URLManager.get_url!(id)
-      if (user_id ==  url.user_id) do
+      if user_id ==  url.user_id do
         json(conn, extract(url))
       else
-        conn
-        |> send_resp(403, "forbidden")
+        conn |> send_resp(403, "forbidden")
       end 
     else
-      conn
-      |> send_resp(401, "You are not connected")
+      conn |> send_resp(401, "You are not connected")
     end
-  end
-
-  def edit(conn, %{"id" => id}) do
-    url = URLManager.get_url!(id)
-    changeset = URLManager.change_url(url)
-    render(conn, "edit.html", url: url, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "url" => url_params}) do
     url = URLManager.get_url!(id)
     {:ok, url} = URLManager.update_url(url, url_params)
-    conn
-    |> put_flash(:info, "Url updated successfully.")
-    |> redirect(to: Routes.url_path(conn, :show, url.id))
+    json(conn, %{url: url.url})
   end
 
   def delete(conn, %{"id" => id}) do
