17fae4e198d5586d763c1a35a507a8034083e30e
Theo Danneels <theo.danneels@telecom-paristech.fr>
2019-06-27 08:56:04 +0200

    Remove render and templates

--- lib/qr_manager_web/controllers/page_controller.ex
+++ lib/qr_manager_web/controllers/page_controller.ex
@@ -2,10 +2,6 @@ defmodule QrManagerWeb.PageController do
   use QrManagerWeb, :controller
   alias QrManager.URLManager
 
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-
   def login(conn, _params) do
     changeset = URLManager.change_url(%URLManager.URL{})
     if conn.assigns[:user] do
@@ -13,6 +9,5 @@ defmodule QrManagerWeb.PageController do
     else
       redirect(conn, external: "https://qrmanager.rfc1149.net/auth/google?scope=email%20profile")
     end
-    # render(conn, "login.html", changeset: changeset)
   end
 end
