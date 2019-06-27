4a0b17a1ddde87d33d0a1667ff2da50d1de49c15
Joel Garde <joel.garde@telecom-paristech.fr>
2019-06-27 11:17:05 +0200

    plus de target blank

--- routing/src/components/DisconnectButton.vue
+++ routing/src/components/DisconnectButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="https://qrmanager.rfc1149.net/auth/signout" target="_blank" class="mh3 f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill  bg-black" @click="connect_api" > Log out </a>
+    <a href="https://qrmanager.rfc1149.net/auth/signout" class="mh3 f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill  bg-black" @click="connect_api" > Log out </a>
 </template>
 
 <script>
