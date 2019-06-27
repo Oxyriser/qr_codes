9c6611ffaeeb0efdb9e00a34a62e0011cc38893f
Ulysse Demay <u.demay@gmail.com>
2019-06-27 11:03:48 +0200

    login fix and clean

--- lib/qr_manager_web/controllers/page_controller.ex
+++ lib/qr_manager_web/controllers/page_controller.ex
@@ -7,18 +7,10 @@ defmodule QrManagerWeb.PageController do
 
   def login(conn, _params) do
     if conn.assigns[:user] do
-      json(conn, %{first_name: conn.assigns.user.first_name})
+      conn
+      |> redirect(to: Routes.page_path(conn, :index))
     else
       redirect(conn, external: "https://qrmanager.rfc1149.net/auth/google?scope=email%20profile")
-    end
-  end
-
-  def logout(conn, _params) do
-    if conn.assigns[:user] do
-      conn
-      |> redirect(to: Routes.session_path(conn, :delete))
-    else
-      redirect(conn, external: "https://qrmanager.rfc1149.net")
     end
   end
 
